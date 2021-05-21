635533bd6deb5cd4fca856757b673d8b37b79a9a
Mateus Miranda <mateusmiranda@gmail.com>
2021-05-21 02:11:38 +0200

    Fixing 0.12 issues

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.24.1"
   namespace  = var.namespace
   name       = var.name
   stage      = var.stage
@@ -11,23 +11,17 @@ module "label" {
 locals {
   enabled      = var.enabled == "true" ? true : false
   require_mfa  = var.require_mfa == "true" ? true : false
-  role_arns    = [values(var.role_arns)]
+  role_arns    = values(var.role_arns)
   role_aliases = [keys(var.role_arns)]
 }
 
 resource "null_resource" "role" {
-  count = length(values(var.role_arns))
+  for_each = var.role_arns
 
   triggers = {
-    account_id = element(split(":", element(local.role_arns, count.index)), 4)
-    role_name = element(
-      split(
-        "/",
-        element(split(":", element(local.role_arns, count.index)), 5),
-      ),
-      1,
-    )
-    alias = element(local.role_aliases, count.index)
+    account_id = element(split(":", each.value), 4)
+    role_name  = element(split("/", each.value), 1)
+    alias      = each.key
   }
 
   lifecycle {
@@ -57,7 +51,7 @@ data "aws_iam_policy_document" "with_mfa" {
       "sts:AssumeRole",
     ]
 
-    resources = [local.role_arns]
+    resources = local.role_arns
 
     condition {
       test     = "Bool"
@@ -84,7 +78,7 @@ data "aws_iam_policy_document" "without_mfa" {
       "sts:AssumeRole",
     ]
 
-    resources = [local.role_arns]
+    resources = local.role_arns
 
     effect = "Allow"
   }
--- output.tf
+++ output.tf
@@ -42,11 +42,8 @@ output "policy_id" {
 
 output "switchrole_urls" {
   description = "List of URL to the IAM console to switch to the roles"
-  value = [formatlist(
-    var.switchrole_url_template,
-    null_resource.role.*.triggers.account_id,
-    null_resource.role.*.triggers.role_name,
-    null_resource.role.*.triggers.alias,
-  )]
+  value = [
+    for role in null_resource.role :
+    format(var.switchrole_url_template, role.triggers.account_id, role.triggers.role_name, role.triggers.alias)
+  ]
 }
-
